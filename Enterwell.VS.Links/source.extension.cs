07c8dbb034137ae0fbdc66418873eb0543635b39
Dorijan <dorijan.haspl@enterwell.net>
2021-03-05 13:24:55 +0100

    removed enterwell name

--- Enterwell.VS.Links/source.extension.cs
+++ Enterwell.VS.Links/source.extension.cs
@@ -8,10 +8,10 @@ namespace Enterwell.VS.Links
     internal sealed partial class Vsix
     {
         public const string Id = "845dfc16-4abc-4c0d-887e-10bd0009d056";
-        public const string Name = "Enterwell Links";
+        public const string Name = "Links";
         public const string Description = @"Visual Studio extension allows quick access to links of interest.";
         public const string Language = "en-US";
-        public const string Version = "1.0.1";
+        public const string Version = "1.0.2";
         public const string Author = "Enterwell";
         public const string Tags = "enterwell, internal, quick links";
     }
